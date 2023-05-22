678433d0e9f8246cadd5551ace7d9e439fbe975a
Agam Suteguh <100204292+agamSuteguh@users.noreply.github.com>
2023-05-22 18:34:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 
-
 const app = express();
 
 const allowedOrigins = ['https://inspiring-faloodeh-d392bd.netlify.app'];
 
 app.use(cors({
-  origin: allowedOrigins
+origin: allowedOrigins
 }));
 
 app.use(express.json());
 
+const mongooseOptions = {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+};
 
-mongoose
-  .connect(
-    process.env.DB_URI
-  )
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+mongoose.connect(process.env.DB_URI, mongooseOptions)
+.then(() => {
+console.log("Connected to database");
+})
+.catch((err) => {
+console.log(err);
 });
 
+app.get("/", (req, res) => {
+res.send("Hello World!");
+});
 
 app.use("/api/auth", require("./routes/auth.js"));
 app.use("/api/siswa", require("./routes/siswa.js"));
@@ -37,10 +37,3 @@ app.use("/api/admin", require("./routes/admin.js"));
 app.use("/api/kelas", require("./routes/kelas.js"));
 app.use("/api/transaksi", require("./routes/transaksi.js"));
 app.use("/api/spp", require("./routes/spp.js"));
-
-
-
-
-
-
-
